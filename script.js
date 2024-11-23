e43e41f5496ee18876b78ad68f07b109c8aabeb6
Ivan Smirnov <vansmrnv05@gmail.com>
2024-11-23 10:19:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ document.getElementById("registrationForm").addEventListener("submit", async fun
 
   try {
       const response = await fetch('http://127.0.0.1:5000/register', {
-          method: 'POST',
+          method: 'POST'
           headers: {
               'Content-Type': 'application/json',
           },
